510d0b3ec51710be2bf8abac8eb2270f17d50cc8
Branislav Buna <46035047+codeHysteria28@users.noreply.github.com>
2021-03-04 18:32:29 +0000

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,36 @@
+# print random string quote
+print("Ken Thompson once said, 'One of my most productive days was throwing away 1000 lines of code'");
+
+
+# print name variable in lower case, first capital char, upper case
+
+name = 'branislav'
+
+print(name)
+print(name.lower())
+print(name.title())
+print(name.upper())
+
+# combining two values 
+
+firstName = 'Branislav'
+lastName = 'Buna'
+
+print(firstName + ' ' + lastName)
+
+# storing two values into variables + making sentece with concatenation about this person
+
+personFirstName = 'John'
+personLastName = 'Doe'
+sentence = personFirstName + ' ' + personLastName + ' is a great programmer'
+
+print(sentence)
+
+# name strip
+
+nickname = ' branko '
+print(nickname)
+print('-' + nickname.lstrip() + '-')
+print('-' + nickname.rstrip() + '-')
+print('-' + nickname.strip() + '-')
+
